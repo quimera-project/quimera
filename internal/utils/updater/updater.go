52b3f317d48a612ebd7a489953bab70ef261f623
PwnedShell <55540142+PwnedShell@users.noreply.github.com>
2022-06-08 19:43:40 +0200

    Bug fix

--- internal/utils/updater/updater.go
+++ internal/utils/updater/updater.go
@@ -11,9 +11,6 @@ import (
 func Check() {
 	if _, err := os.Stat(env.Config.WorkshopDir); os.IsNotExist(err) {
 		live.Printer.Infof("%s does not exists. Downloading workshop...", env.Config.WorkshopDir)
-		if err != nil {
-			live.Printer.Fatalf("%v", err)
-		}
 		if _, err = git.PlainClone(env.Config.WorkshopDir, false, &git.CloneOptions{
 			URL:      "https://github.com/quimera-project/quimera-workshop",
 			Progress: nil,
